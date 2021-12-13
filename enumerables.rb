87500091f9bdde31c8005338ab4bb4bb679e9ebf
yakzaetz <yakzaetz@gmail.com>
2021-12-13 17:37:46 -0500

    my_zip final  attempt

--- enumerables.rb
+++ enumerables.rb
@@ -1,3 +1,4 @@
+require "byebug"
 class Array
     def my_each
         i = 0
@@ -32,7 +33,35 @@ class Array
         count == self.length
     end
 
+    def my_flatten
+        # return [self] if !self.is_a?(Array)
 
+        flattened = []
+        self.each do |ele|
+            # debugger
+            if !ele.is_a?(Array)
+                flattened << ele
+            else
+                flattened += ele.my_flatten
+            end 
+        end 
+        flattened
+    end
+
+    def my_zip(*array)
+        new_arr = Array.new(self.length){Array.new}
+
+        new_arr.each.with_index do |sub_arr, i|
+            sub_arr << self[i]
+            array.each do |other_sub|
+                sub_arr << other_sub[i]
+            end 
+        end
+        
+        # l = self.map(&:length).max 
+        # new_array = self.map { |sub| sub.values_at(0...l) }
+        # new_array.transpose
+    end
 
 
 
@@ -48,4 +77,15 @@ p arr.my_select { |num| num > 1 }
 p arr.my_any? { |num| num > 1 }
 p arr.my_all? { |num| num > 1 }
 
+p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
 
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+# p a.my_zip(b)
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+# c = [10, 11, 12]
+# d = [13, 14, 15]
+# [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
