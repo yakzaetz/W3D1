1ae0cb29748011a828ad41e8c9ab460c105b2008
qienzgao <qienzgao@gmail.com>
2021-12-13 16:21:48 -0500

    my project

--- enumerables.rb
+++ enumerables.rb
@@ -7,8 +7,45 @@ class Array
         end
         return self
     end
+    
+    def my_reject(&prc)
+        self.select { |ele|!prc.call(ele) }
+    end 
+
+    def my_select(&prc)
+        self.reject { |ele| !prc.call(ele) }
+    end
+
+    def my_any?(&prc)
+        count = 0
+        self.each do |ele|
+            count += 1 if prc.call(ele)
+        end
+        count > 0 
+    end
+    
+    def my_all?(&prc)
+        count = 0
+        self.each do |ele|
+            count += 1 if prc.call(ele)
+        end
+        count == self.length
+    end
+
+    
+
+
+
+
+
 end
 
 #spooky shit
 arr = [1, 2, 3]
-arr.my_each
+p arr.my_each
+p arr.my_reject { |num| num > 1 }
+p arr.my_select { |num| num > 1 }
+p arr.my_any? { |num| num > 1 }
+p arr.my_all? { |num| num > 1 }
+
+
